6f486513d5194cdf5a35d17c5507e60420aac3b2
Tobias Leinß <tobias.leinss@mway.io>
2023-06-09 19:11:44 +0200

    Deploy coins to sepolia

--- contracts_tokens/README.md
+++ contracts_tokens/README.md
@@ -1,13 +1,14 @@
-# Sample Hardhat Project
+# Liquidity Tokens
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
-
-Try running some of the following tasks:
+Check `package.json` for scripts.
 
 ```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.ts
+$ npx hardhat run --network sepolia scripts/deploy.ts
+Deploying contracts with the account: 0x4bFC74983D6338D3395A00118546614bB78472c2
+Account balance before deployment: 9.906652682783500167 ETH
+USDC, USDC deployed to 0x4140c268adAE01bb62F1Aa8D043000c36e692731
+Good Coin, GCOIN deployed to 0x3311bAcC036f0007638CF63820a1bdeE00903678
+Bad Coin, BCOIN deployed to 0x662F26b3A3Ddd5E91D5e6B85Fd9E253a873e33E6
+Account balance after deployment: 9.906652682783500167 ETH
+✨  Done in 44.72s.
 ```
--- contracts_tokens/scripts/deploy.ts
+++ contracts_tokens/scripts/deploy.ts
@@ -19,18 +19,35 @@ async function main() {
 
   const Coin = await ethers.getContractFactory("Coin")
 
-  const deployedCoins = await Promise.all(
-    coins.map(async (coin) => {
-      const c = await Coin.deploy(coin.initialSupply, coin.name, coin.symbol)
-      return c.waitForDeployment()
-    })
+  const coin0 = await Coin.deploy(
+    coins[0].initialSupply,
+    coins[0].name,
+    coins[0].symbol
+  )
+  await coin0.waitForDeployment()
+  console.log(
+    `${await coin0.name()}, ${await coin0.symbol()} deployed to ${coin0.target}`
   )
 
-  for (const coin of deployedCoins) {
-    console.log(
-      `${await coin.name()}, ${await coin.symbol()} deployed to ${coin.target}`
-    )
-  }
+  const coin1 = await Coin.deploy(
+    coins[1].initialSupply,
+    coins[1].name,
+    coins[1].symbol
+  )
+  await coin1.waitForDeployment()
+  console.log(
+    `${await coin1.name()}, ${await coin1.symbol()} deployed to ${coin1.target}`
+  )
+
+  const coin2 = await Coin.deploy(
+    coins[2].initialSupply,
+    coins[2].name,
+    coins[2].symbol
+  )
+  await coin2.waitForDeployment()
+  console.log(
+    `${await coin2.name()}, ${await coin2.symbol()} deployed to ${coin2.target}`
+  )
 
   console.log(
     `Account balance after deployment: ${ethers.formatEther(balance)} ETH`
